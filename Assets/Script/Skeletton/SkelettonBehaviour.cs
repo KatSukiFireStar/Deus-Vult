3f27632735cb0594678581656bcfee9b32171008
KaTSukiFireStar <78344193+KatSukiFireStar@users.noreply.github.com>
2024-12-03 09:36:05 +0100

    remove some using

--- Assets/Script/Skeletton/SkelettonBehaviour.cs
+++ Assets/Script/Skeletton/SkelettonBehaviour.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.ComponentModel;
 using EventSystem.SO;
-using UnityEditor.UI;
 using UnityEngine;
 
 public class SkelettonBehaviour : MonoBehaviour
